bb3d9caf5e0c587ecdc4339d717ce4aec04e6d20
Moein <m.z251376@gmail.com>
2026-09-15 04:49:13 +0330

    Add User class

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-name = input("What is your name? ")
-print(f"Hello, {name}!")
-age = int(input("How old are you? "))
-print(f"You are {age} years old.")
- 
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+            print(f"My name is {self.name} and my age is {self.age}")
+
+user = User("Moein", 29)
+user.introduce()
